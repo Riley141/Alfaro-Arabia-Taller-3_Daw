1832669b569387a8aea07a4901637aebe22f057f
Angelus <100987588+Riley141@users.noreply.github.com>
2022-10-27 17:24:09 -0600

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,4 +1,23 @@
 var bbtt = document.getElementById("boton");
+localStorage.setItem('formData', (formData));
 bbtt.addEventListener("click",agregar);
+dispData();
+e.preventDefault();
+function dispData(){
+if(local Storage.getItem('formData')){
+}
+ <table>
+ <tbody>
+ <tr>
+         <td>Nombre De identidad</td> 
+          <td>$[Fnombre]</td>
+   <td>Tipo Tarjeta</td> 
+   <td>$[tar]</td>
+  <td>Numero de La Tarjeta</td>
+  <td>$[NumTar]</td>
+  <td>Fecha de Caducidad</td>
+  <td>$[FCadu]</td>
+</tr>
+
 
 
